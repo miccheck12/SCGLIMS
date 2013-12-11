b963a1ace61e14d654e477f8bec0b6a266711f3d
Ino de Bruijn <ino.debruijn@scilifelab.se>
2013-12-11 18:31:17 +0100

    Add DNALibrary Generic relations. Add ExtractedCell and DNALibrary auto replicate numbering.

--- lims_project/lims/admin.py
+++ lims_project/lims/admin.py
@@ -1,5 +1,33 @@
 from django.contrib import admin
-from lims.models import Collaborator, Sample, SampleType, SampleLocation, StorageLocation
+from django.contrib.contenttypes import generic
 
-for model in [Collaborator, Sample, SampleType, SampleLocation, StorageLocation]:
+from lims.models import Collaborator, Sample, SampleType, SampleLocation, \
+    StorageLocation, Protocol, ExtractedCell, ExtractedDNA, QPCR, RTMDA, SAGPlate, \
+    SAGPlateDilution, DNALibrary, SequencingRun, Metagenome, Primer, Amplicon, SAG, PureCulture
+
+standard_models = [Collaborator, Sample, SampleType, SampleLocation,
+              StorageLocation, Protocol, QPCR,
+              RTMDA, SAGPlate, SAGPlateDilution, SequencingRun,
+              Metagenome, Primer, Amplicon, SAG, PureCulture]
+
+for model in standard_models:
     admin.site.register(model)
+
+class DNALibraryAdmin(admin.ModelAdmin):
+    exclude = ('content_type',)
+
+    def formfield_for_choice_field(self, db_field, request, **kwargs):
+            if db_field.name == "object_id":
+                objects = list(Amplicon.objects.all()) + list(SAG.objects.all()) + \
+                    list(PureCulture.objects.all())
+                kwargs['choices'] = tuple([(o.id, o.id) for o in objects])
+                kwargs['empty_label'] = "---------"
+                kwargs['blank'] = False
+            return super(DNALibraryAdmin, self).formfield_for_choice_field(db_field, request, **kwargs)
+
+class ExtractedCellAdmin(admin.ModelAdmin):
+    readonly_fields = ('replicate_number',)
+
+admin.site.register(DNALibrary, DNALibraryAdmin)
+admin.site.register(ExtractedCell, ExtractedCellAdmin)
+admin.site.register(ExtractedDNA, ExtractedCellAdmin)
--- lims_project/lims/models.py
+++ lims_project/lims/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
 
 # Create your models here.
 class Collaborator(models.Model):
@@ -15,32 +17,38 @@ class Collaborator(models.Model):
 
 class SampleType(models.Model):
     name = models.CharField(max_length=30)
-    description = models.TextField()
+    description = models.TextField(blank=False)
 
     def __unicode__(self):
-        return "%s %s" % (self.name, self.description)
+        return "%s" % (self.name)
 
 class SampleLocation(models.Model):
     name = models.CharField(max_length=30)
-    description = models.TextField()
+    description = models.TextField(blank=False)
+
+    def __unicode__(self):
+        return "%s" % (self.name)
 
 class StorageLocation(models.Model):
     name = models.CharField(max_length=30)
-    description = models.TextField()
+    description = models.TextField(blank=False)
+
+    def __unicode__(self):
+        return "%s" % (self.name)
 
 class Sample(models.Model):
-    id = models.CharField("ID", max_length=20, primary_key=True)
+    uid = models.CharField("UID", max_length=20, unique=True)
 
     collaborator = models.ForeignKey(Collaborator)
     sample_type = models.ForeignKey(SampleType)
     sample_location = models.ForeignKey(SampleLocation)
     storage_location = models.ForeignKey(StorageLocation)
 
-    temperature = models.DecimalField(u"Temperature \u00B0C", max_digits=3,
+    temperature = models.DecimalField(u"Temperature \u00B0C", max_digits=10,
         decimal_places=2)
-    ph = models.DecimalField(" pH", max_digits=3, decimal_places=2)
-    salinity = models.DecimalField("Salinity unit(?))", max_digits=3, decimal_places=2)
-    depth = models.DecimalField(max_digits=3, decimal_places=2)
+    ph = models.DecimalField(" pH", max_digits=10, decimal_places=2)
+    salinity = models.DecimalField("Salinity unit(?))", max_digits=10, decimal_places=2)
+    depth = models.DecimalField("Depth (m)", max_digits=10, decimal_places=2)
     gps = models.CharField("GPS", max_length=30)
     shipping_method = models.CharField(max_length=30)
     storage_medium = models.CharField(max_length=30)
@@ -49,45 +57,83 @@ class Sample(models.Model):
         choices=((1, 1), (2, 2), (3, 3), (4, 4)))
     status = models.CharField(max_length=8,
         choices=(('new', 'new'), ('used', 'used'), ('finished','finished')))
-    notes = models.TextField()
+    notes = models.TextField(blank=True)
 
     @property
     def barcode(self):
-        return "SA:" + str(self.id)
+        return "SA:" + str(self.uid)
+
+    def __unicode__(self):
+        return "%s" % (self.uid)
+
 
 class Protocol(models.Model):
-    info = models.TextField()
+    name = models.CharField(max_length=30)
     revision = models.CharField(max_length=30)
     link = models.CharField(max_length=100)
+    notes = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return "%s" % (self.name)
 
 class ExtractedCell(models.Model):
     sample = models.ForeignKey(Sample)
-    replicate_number = models.IntegerField()
+    uid = models.CharField("UID", max_length=20, unique=True, editable=False)
+    replicate_number = models.IntegerField(default="Automatically generated")
     protocol = models.ForeignKey(Protocol)
     storage_location = models.ForeignKey(StorageLocation)
-    notes = models.TextField()
+    notes = models.TextField(blank=True)
 
     @property
     def barcode(self):
-        return "EC:" + str(self.sample.id)
+        return "EC:%s" % str(self.uid)
+
+    def save(self):
+        self.replicate_number = ExtractedCell.objects.filter(sample=self.sample.id).count() + 1
+        self.uid = "%s_%s" % (self.sample.uid, self.replicate_number)
+        super(ExtractedCell, self).save()
+
+    def __unicode__(self):
+        return self.uid
 
 class ExtractedDNA(models.Model):
     sample = models.ForeignKey(Sample)
-    replicate_number = models.IntegerField()
+    uid = models.CharField("UID", max_length=20, unique=True, editable=False)
+    replicate_number = models.IntegerField(default="Automatically generated")
     protocol = models.ForeignKey(Protocol)
     storage_location = models.ForeignKey(StorageLocation)
-    notes = models.TextField()
-    extracted_cell = models.ForeignKey(ExtractedCell)
+    notes = models.TextField(blank=True)
+    extracted_cell = models.ForeignKey(ExtractedCell, null=True, blank=True)
 
     @property
     def barcode(self):
-        return "ED:" + str(self.sample.id)
+        return "ED:" + str(self.uid)
+
+    def save(self):
+        self.replicate_number = ExtractedDNA.objects.filter(sample=self.sample.id).count() + 1
+        self.uid = "%s_%s" % (self.sample.uid, self.replicate_number)
+        super(ExtractedDNA, self).save()
+
+    class Meta:
+        verbose_name = "Extracted DNA"
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.uid
 
 class QPCR(models.Model):
     report = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = "QPCR"
+        verbose_name_plural = "QPCRs"
+
 class RTMDA(models.Model):
     report = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "RT-MDA kinetics"
+        verbose_name_plural = verbose_name
 
 class SAGPlate(models.Model):
     report = models.CharField(max_length=100)
@@ -102,7 +148,85 @@ class SAGPlate(models.Model):
     def barcode(self):
         return "SP:" + str(self.sample.id)
 
+    class Meta:
+        verbose_name = "SAG plate"
+        verbose_name_plural = verbose_name + "s"
+
 class SAGPlateDilution(models.Model):
     sag_plate = models.ForeignKey(SAGPlate)
     dilution = models.CharField(max_length=100)
     qpcr = models.ForeignKey(QPCR)
+
+    class Meta:
+        verbose_name = "SAG plate dilution"
+        verbose_name_plural = verbose_name + "s"
+
+class DNALibrary(models.Model):
+    limit = models.Q(app_label = 'lims', model = 'amplicon') | \
+            models.Q(app_label = 'lims', model = 'metagenome') | \
+            models.Q(app_label = 'lims', model = 'sag') | \
+            models.Q(app_label = 'lims', model = 'pureculture')
+    content_type = models.ForeignKey(ContentType, limit_choices_to=limit)
+    object_id = models.PositiveIntegerField(choices=(('5','5'),))
+    current_object = generic.GenericForeignKey('content_type', 'object_id')
+
+    buffer = models.CharField(max_length=100)
+    i7 = models.CharField(max_length=100)
+    i5 = models.CharField(max_length=100)
+    sample_name_on_platform = models.CharField(max_length=100)
+
+    #fastq_files
+    protocol = models.ForeignKey(Protocol)
+    storage_location = models.ForeignKey(StorageLocation)
+
+    class Meta:
+        verbose_name = "DNA library"
+        verbose_name_plural = verbose_name[:-1] + "ies"
+
+class SequencingRun(models.Model):
+    date = models.DateField()
+    machine = models.CharField(max_length=100)
+    report = models.CharField(max_length=100)
+    folder = models.CharField(max_length=100)
+    notes = models.TextField()
+    dna_library = models.ManyToManyField(DNALibrary)
+
+    protocol = models.ForeignKey(Protocol)
+
+class Metagenome(models.Model):
+    extracted_dna = models.ForeignKey(ExtractedDNA)
+    sample = models.ForeignKey(Sample)
+    diversity_report = models.CharField(max_length=100)
+    dna_library = generic.GenericRelation(DNALibrary)
+
+class Primer(models.Model):
+    sequence = models.TextField()
+    storage_location = models.ForeignKey(StorageLocation)
+    tmelt = models.DecimalField(u"tmelt \u00B0C", max_digits=10,
+                                decimal_places=2)
+    concentration = models.CharField(max_length=100)
+    stock = models.PositiveIntegerField()
+
+class Amplicon(models.Model):
+    extracted_dna = models.ForeignKey(ExtractedDNA)
+    sample = models.ForeignKey(Sample)
+    diversity_report = models.CharField(max_length=100)
+    buffer = models.CharField(max_length=100)
+    notes = models.TextField()
+    primer = models.ManyToManyField(Primer)
+    dna_library = generic.GenericRelation(DNALibrary)
+
+class SAG(models.Model):
+    sag_plate_dilution = models.ForeignKey(SAGPlateDilution)
+    well = models.CharField(max_length=3)
+    concentration = models.CharField(max_length=100)
+    dna_library = generic.GenericRelation(DNALibrary)
+
+    class Meta:
+        verbose_name = "SAG"
+        verbose_name_plural = "SAGs"
+
+class PureCulture(models.Model):
+    extracted_dna = models.ForeignKey(ExtractedDNA)
+    concentration = models.CharField(max_length=100)
+    dna_library = generic.GenericRelation(DNALibrary)
